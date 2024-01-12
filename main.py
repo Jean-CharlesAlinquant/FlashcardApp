4371829af86f006e2bf7582231576387807ceeba
Jean-CharlesAlinquant <jcalinquant@hotmail.com>
2024-01-12 13:27:09 +0000

    Displaying random cards

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("./data/french_words.csv")
+data_dict = data.to_dict(orient="records")
+
+# ---------------------------- Generate Random Word ------------------------------- #
+
+
+def next_card():
+    word = random.choice(data_dict)["French"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=word)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -11,17 +26,19 @@ canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_img)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Row 1
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_btn.grid(row=1, column=0)
 
 right_img = PhotoImage(file="./images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0)
+right_btn = Button(image=right_img, highlightthickness=0, command=next_card)
 right_btn.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
